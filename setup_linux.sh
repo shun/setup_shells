0d5096680c73b2954e32d47178b8657fcd2f2f0f
Shunsuke Kudo <kudo.shunsuke.xx@gmail.com>
2019-03-12 22:45:55 +0900

    up

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,11 +2,18 @@
 set -e
 
 # add google chrome repository
-curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
+which google-chrome-stable > /dev/null #2&>1
+if [ $? -ne 0 ]; then
+	curl https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+	echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
+fi
+
+# paper-icon-theme
+add-apt-repository -y -u ppa:snwh/ppa
 
 apt update && apt upgrade -y && \
 apt install -y \
+	arc-theme \
 	build-essential \
 	ctags \
 	curl \
@@ -20,6 +27,7 @@ apt install -y \
 	htop \
 	i3 \
 	openssh-server \
+	paper-icon-theme \
 	python3 \
 	python3-pip \
 	tig \
@@ -27,6 +35,10 @@ apt install -y \
 	vim \
 	wget \
 	xsel \
+
+if [ ! -e ~/.local/bin ]; then
+	mkdir ~/.local/bin
+fi
 
 # install pt
 go get -u github.com/monochromegane/the_platinum_searcher/...

5ef7fa1483b58314f9c7a37e09d9adfa1d24516e
Shunsuke Kudo <kudo.shunsuke.xx@gmail.com>
2019-03-16 18:34:13 +0900

    Update setup_linux.sh

--- setup_linux.sh
+++ setup_linux.sh
@@ -56,10 +56,12 @@ go get -u github.com/monochromegane/the_platinum_searcher/...
 
 # install peco
 PECO_VER=v0.5.3
-curl -L https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz | tar -zxv -C ~/.local/bin/ && mv ~/.local/bin/peco_linux_amd64/peco ~/.local/bin/ && rm -rf ~/.local/bin/peco_linux_amd64/
+curl -L https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz | tar -zxv -C ~/.local/bin/ && \
+	mv ~/.local/bin/peco_linux_amd64/peco ~/.local/bin/ \
+	&& rm -rf ~/.local/bin/peco_linux_amd64/
 
 # setup docker
-curl -L https://raw.githubusercontent.com/shun/ubuntu-docker-setup/master/install_docker.sh | sudo bash
+curl -L https://raw.githubusercontent.com/shun/ubuntu-docker-setup/master/install_docker.sh | bash -
 python3 -m pip install docker-compose
 CTOP_VER=0.7.2
 wget https://github.com/bcicen/ctop/releases/download/v$CTOP_VER/ctop-$CTOP_VER-linux-amd64 -O ~/.local/bin/ctop

3392059e9e1bebf3351caf2a1afa139f8f5cb027
KUDO Shunsuke <kudo.shunsuke.xx@gmail.com>
2021-01-25 23:31:51 +0900

    up

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,76 +1,61 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-if [ ! -e ~/workspace  ]; then
-    mkdir -p ~/workspace/gitrepo
-    mkdir -p ~/workspace/dev
-    mkdir -p ~/workspace/gitprompt
-fi
-
-
-wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O $HOME/workspace/gitrepo/gitprompt/git-completion.bash
-wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O $HOME/workspace/gitrepo/gitprompt/git-prompt.sh
-
-cd $HOME/workspace/gitrepo
-if [ ! -e myscript ]; then
-    git clone git@github.com:shun/myscript.git
-fi
-
-if [ ! -e dotconfig ]; then
-    git clone git@github.com:shun/dotconfig.git
-
-    cd dotconfig
-    bash setup.sh
-fi
-
-
-# check brew install status
-command_='/usr/local/bin/brew'
-type $command_  > /dev/null 2>&1
-
-if [ ! $? = 0 ]; then
+setup_brew() {
+  type brew > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    echo "install brew..."
     # install brew
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update
-fi
+  fi
+  brew update
 
-brew install \
-    boost \
+  brew install \
+    anyenv \
     cmake \
+    coreutils \
     curl \
+    fzf \
+    ghq \
     git \
-    global --wigh-pygments \
+    global \
+    gnu-sed \
     go \
-    gnu-erd \
     highlight \
-    llvm \
-    neovim \
     p7zip \
-    peco \
-    pt \
-    python3 \
-    ranger \
+    ripgrep \
     tig \
     wget
 
-brew cask install java
+  brew upgrade
+}
 
-pip3 install -U pip
-pip3 install neovim
-pip3 install neovim-remote
+setup_shell() {
+  if [ ! "$SHELL" = "/usr/local/bin/bash" ]; then
+    chsh -s /usr/local/bin/bash
+  fi
+}
 
-# install nodejs, typescript
-which node
-if [ ! $? -eq 0 ]; then
-    brew install npm
-    sudo npm install -g typescript
-fi
+#setup_neovim() {
+#}
 
-# install rust lang
-which ructc
-if [ ! $? -eq 0 ]; then
-    curl https://sh.rustup.rs -sSf | sh
-    source $HOME/.cargo/env
-    cargo install rustfmt
-    cargo install racer
-fi
+setup_envs() {
+  anyenv install nodenv
 
+  ANYENV_ROOT=$(anyenv root)
+  mkdir -p ${ANYENV_ROOT}/plugins
+  if [ ! -e ${ANYENV_ROOT}/plugins/anyenv-update ]; then
+    git clone https://github.com/znz/anyenv-update.git ${ANYENV_ROOT}/plugins/anyenv-update
+  fi
+
+  NODENV_ROOT=$(nodenv root)
+  mkdir -p ${NODENV_ROOT}/plugins
+  if [ ! -e "${NODENV_ROOT}/plugins/nodenv-default-packages" ]; then
+    git clone https://github.com/nodenv/nodenv-default-packages.git "${NODENV_ROOT}/plugins/nodenv-default-packages"
+  fi
+
+  touch $(nodenv root)/default-packages
+}
+#setup_brew
+#setup_shell
+#setup_gotools
+setup_envs
